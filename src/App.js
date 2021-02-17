505cc9bae965b9d93b6f51e5fac3084c10fc84b9
Daniel McKinney <adrmckinney@gmail.com>
2021-02-17 00:57:25 -0500

    first commit. Building the framework.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import Experience from './components/Experience'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App h-screen'>
+      <Header />
+      <div className='bg-gray-900 h-full flex flex-col sm:flex-row p-4'>
+        <Experience />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Experience.js
@@ -0,0 +1,15 @@
+
+const Experience = () => {
+  return (
+    <div className='flex flex-col sm:flex-row p-4'>
+      <div className='flex justify-center sm:justify-end items-center flex-1 max-h-36'>
+        <h3 className='text-white text-2xl text-center sm:text-right max-w-xs font-bold m-6'>Community Organizing</h3>
+      </div>
+      <div className='flex-1 max-h-48 sm:max-h-36 justify-center'>
+        <p className='text-white max-w-md m-6 text-center sm:text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam fugiat illo aliquam optio neque sit architecto laudantium eveniet, quibusdam dolorum animi culpa impedit mollitia quaerat libero! Dolores, dolor deserunt? Dolores!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
